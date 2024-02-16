3f79e26296aa914b0688733be026cb363f339f37
rare-only <146336796+rare-only@users.noreply.github.com>
2024-02-16 20:39:20 +0530

    Add files via upload

---
+++ Parallel Subtractor [Day-05]/parallel_subtractor.v
@@ -0,0 +1,18 @@
+module parallel_subtractor(a,b,ci,d,co);
+input a,b,ci;
+output d,co;
+assign d=a^b^ci;
+assign co=((~a&b)|~(a^b)&ci);
+endmodule    
+
+module parallel_full_subtractor(
+    input [3:0] a,b,
+    input ci,
+    output[3:0] d,
+    output co);
+    wire [2:0] w;
+ parallel_subtractor fa1 (a[0],!b[0],ci,d[0],w[0]);
+ parallel_subtractor fa2 (a[1],!b[1],w[0],d[1],w[1]);
+ parallel_subtractor fa3 (a[2],!b[2],w[1],d[2],w[2]);
+ parallel_subtractor fa4 (a[3],!b[3],w[2],d[3],co);
+ endmodule
---
+++ Parallel Subtractor [Day-05]/parallel_subtractor_tb.v
@@ -0,0 +1,29 @@
+`include "parallel_subtractor.v"
+
+module parallel_subtractor_tb;
+reg[3:0]a;
+reg [3:0]b;
+reg ci;
+wire [3:0]d;
+wire co;
+
+parallel_full_subtractor ps1(a,b,ci,d,co);
+
+initial 
+begin
+$dumpfile("output.vcd");
+$dumpvars(0,parallel_subtractor_tb);
+$monitor("At time %0t: a=%b,b=%b,ci=%b,d=%0b,co=%b", $time,a,b,ci,d,co);
+end
+initial
+begin
+       
+
+     a = 4'b0110; b = 4'b1001; ci = 1'b1;     #10;   
+     a = 4'b0111; b = 4'b1110; ci = 1'b1;      #10; 
+     a = 4'b1000; b = 4'b0100; ci = 1'b1;      #10; 
+     a = 4'b1010; b = 4'b1000; ci = 1'b1;      #10; 
+    $finish();
+    end
+
+endmodule    
